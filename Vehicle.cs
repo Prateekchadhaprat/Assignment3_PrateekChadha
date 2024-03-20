bb38011fa6883c6738aa1e9fa5687028486a3aac
Prateek <prateek.9n@gmail.com>
2024-03-20 11:25:48 -0400

    worked on vehicle class methods.

--- Vehicle.cs
+++ Vehicle.cs
@@ -11,6 +11,19 @@ namespace Assignment3_PrateekChadha
         public string Model { get; set; }
         public string Manufacturer { get; set; }
         public int Year { get; set; }
-        public double RentalPrice { get; set; } 
+        public double RentalPrice { get; set; }
+
+        public Vehicle(string model, string manufacturer, int year, double rentalPrice)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Year = year;
+            RentalPrice = rentalPrice;
+        }
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice}");
+        }
     }
+
 }
