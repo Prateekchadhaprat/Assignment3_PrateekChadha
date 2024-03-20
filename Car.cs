25596ac6e86c74373d3b3ed5ef56e08811954bae
Prateek <prateek.9n@gmail.com>
2024-03-20 11:41:44 -0400

    created override displaydetails function to display car properties and additional properties from vehicle

--- Car.cs
+++ Car.cs
@@ -13,4 +13,19 @@ namespace Assignment3_PrateekChadha
         public string Transmission { get; set; }
         public bool Convertible { get; set; }
 
-       
+        public Car(string model, string manufacturer, int year, double rentalPrice,
+               int seats, string engineType, string transmission, bool convertible)
+        : base(model, manufacturer, year, rentalPrice)
+        {
+            Seats = seats;
+            EngineType = engineType;
+            Transmission = transmission;
+            Convertible = convertible;
+        }
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Seats: {Seats}, Engine Type: {EngineType}, Transmission: {Transmission}, Convertible: {Convertible}");
+        }
+    }
+}
