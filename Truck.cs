89d78eed9a1831c34ebf5c7bab1b3b3285b9cd6a
Prateek <prateek.9n@gmail.com>
2024-03-22 17:09:09 -0400

    Worked on truck class, created constructor and override method to display truck details.

--- Truck.cs
+++ Truck.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Assignment3_PrateekChadha
 {
-    internal class Truck
+    public class Truck : Vehicle
     {
+        public int Capacity { get; set; }
+        public string TruckType { get; set; }
+        public bool FourWheelDrive { get; set; }
+        public Truck(string model, string manufacturer, int year, double rentalPrice,
+                 int capacity, string truckType, bool fourWheelDrive)
+        : base(model, manufacturer, year, rentalPrice)
+        {
+            Capacity = capacity;
+            TruckType = truckType;
+            FourWheelDrive = fourWheelDrive;
+        }
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Capacity: {Capacity}, Truck Type: {TruckType}, Four Wheel Drive: {FourWheelDrive}");
+        }
     }
 }
