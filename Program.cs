a403077705e6b2124857770e3e00fc8f712731c1
Prateek <prateek.9n@gmail.com>
2024-03-22 18:55:26 -0400

    Created instances of different vehicles, implented foreach loop for Display details of each vehicle in the fleet

--- Program.cs
+++ Program.cs
@@ -12,4 +12,32 @@ namespace Assignment3_PrateekChadha
         {
             
             Car car1 = new Car("Supra Mark iv", "Toyota", 2005, 50.0, 5, "Gasoline", "Manual", false);
+            Truck truck1 = new Truck("Bronco", "Ford", 2010, 80.0, 4, "Pickup", true);
+            Motorcycle motorcycle1 = new Motorcycle("H2R", "Kawasaki", 2020, 30.0, 1000, "Gasoline", true);
+
             
+            RentalAgency agency = new RentalAgency(10);
+
+            
+            agency.AddVehicle(car1);
+            agency.AddVehicle(truck1);
+            agency.AddVehicle(motorcycle1);
+
+            foreach (var vehicle in agency.Fleet)
+            {
+                if (vehicle != null)
+                {
+                    vehicle.DisplayDetails();
+                }
+            }
+            
+            agency.RentVehicle("Supra Mark iv", 3);
+
+            
+            agency.RemoveVehicle("Bronco");
+
+            
+            Console.WriteLine($"Total Revenue: {agency.TotalRevenue}");
+        }
+    }
+    }
