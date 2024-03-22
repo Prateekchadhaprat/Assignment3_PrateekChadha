0a8da0f9f2e428c4ccc6c1321820306db60d1ed8
Prateek <prateek.9n@gmail.com>
2024-03-22 18:01:07 -0400

    working on motorcyce class

--- Motorcycle.cs
+++ Motorcycle.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Assignment3_PrateekChadha
 {
-    internal class Motorcycle
+    public class Motorcycle : Vehicle
     {
+        // Additional properties for Motorcycle
+        public int EngineCapacity { get; set; }
+        public string FuelType { get; set; }
+        public bool HasFairing { get; set; }
     }
 }
